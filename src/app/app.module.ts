a047b928a8eb038dbc403dad8f8d0e3304733d36
Pedro Trudes <pedro.jtrudes@gmail.com>
2022-05-30 18:19:59 -0300

    subindo arquivos de criação de conta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { LoginComponent } from './components/form/login/login.component';
 import { MatInputModule} from '@angular/material/input';
+import { CadastroComponent } from './components/form/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
