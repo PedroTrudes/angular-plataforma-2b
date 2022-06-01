dfc6184da7139655d67cadec7f8947e50fd493da
Pedro Trudes <pedro.jtrudes@gmail.com>
2022-06-01 15:37:50 -0300

    subindo parte de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CadastroComponent } from './components/form/cadastro/cadastro.component';
+import { LoginComponent } from './components/form/login/login.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'sing-in', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
